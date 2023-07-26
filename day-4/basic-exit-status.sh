192ab0788b9780e4070ba6a3b43fd5650cdc3f11
mno2023 <mnomkar2023@gmail.com>
2023-07-27 01:30:25 +0530

    Added script to demo exit status

---
+++ day-4/basic-exit-status.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Author : Omkar Sharma MN
+# Date Created : 07/14/2023
+# Description : Shell script to display Hello World
+# Date Modified : 07/14/2023
+echo "###############"
+echo Exit status Demo
+echo "###############"
+echo
+
+echo "Enter a simple unix command (Enter date command) : "
+read cmd
+`$cmd &> /dev/null `
+status=$?
+
+if [ $status == 0 ]
+then
+	echo "****** Command was successfull *******"
+	echo "Output of $cmd is : `$cmd`"
+else
+	echo "****** Command failed *******"
+fi
+
