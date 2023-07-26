51bcf75131b0ca6f7101569564cd3d63031f79f1
mno2023 <mnomkar2023@gmail.com>
2023-07-26 23:59:51 +0530

    Added for loop examples

---
+++ day-3/basic-for.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Author : Omkar Sharma MN
+# Date Created : 07/14/2023
+# Description : Shell script to display Hello World
+# Date Modified : 07/14/2023
+echo "##################"
+echo Basic for statement
+echo "##################"
+
+#for variable in value1 value2 value3 (-> SEPARATED BY SPACE )
+#do
+#	command1
+#	command2
+#done
+
+for item in 1 2 3 4 5
+do
+	echo "Welcome to Bash Shell $item time, for loop."
+done
